8d698736541d72aedf95d42a0b78bd660ef6420e
miliperuzotti <miliperuzotti@gmail.com>
2026-05-30 22:21:45 -0300

    primer declaracion de variables y const

---
+++ practica-16/js/archivo.js
@@ -0,0 +1,8 @@
+//declaracion variables
+let miNombre="Milagros";
+let miApellido="Peruzotti"
+let combinados=miNombre+miApellido
+let edad=18
+//declaracion constantes
+const una="Universidad Nacional de las Artes"
+console.log(miNombre, miApellido, edad, una)
