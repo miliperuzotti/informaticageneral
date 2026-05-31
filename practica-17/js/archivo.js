e0f875fe00ab9091dca9d5fae2142e3241d75a0b
miliperuzotti <miliperuzotti@gmail.com>
2026-05-30 23:12:30 -0300

    practica 17
    me costo un monton restarle un porcentaje a la variable, no se si estara bien resuelto pero funciona asi que ojala si

---
+++ practica-17/js/archivo.js
@@ -0,0 +1,4 @@
+//declaracion variables
+let precio=2000
+let descuento=25
+console.log(precio, descuento, precio-=precio/=100/descuento)//no se si esta operacion matematica esta bien hecha pero ojalá si
